cd98257349cc41198a0fca5787df11f0e7700ccb
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-10 00:34:33 -0300

    test: should render loading component on page moutn

---
+++ src/test/presentation/home/home.spec.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react'
+
+import { Home } from '@/presentation/pages/home/home'
+
+describe('Home Page', () => {
+  it('should render loading component on page mount', () => {
+    const { getByTestId } = render(<Home />)
+
+    const loadingPage = getByTestId('loading-page')
+
+    expect(loadingPage.childElementCount).toBe(1)
+  })
+})

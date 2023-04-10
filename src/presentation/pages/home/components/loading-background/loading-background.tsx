0440255033d8e32f05a2f4875bc95258c2cdc416
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-10 00:22:41 -0300

    fix: page region

--- src/presentation/pages/home/components/loading-background/loading-background.tsx
+++ src/presentation/pages/home/components/loading-background/loading-background.tsx
@@ -3,13 +3,13 @@ import { Spinner } from './spinner'
 
 export const LoadingBackground = () => {
   return (
-    <div className="bg-loading-background bg-top bg-cover bg-no-repeat min-h-screen flex justify-center items-center flex-col">
+    <main className="bg-loading-background bg-top bg-cover bg-no-repeat min-h-screen flex justify-center items-center flex-col">
       <div className="border-4 border-red-600 p-1 bg-gray-200 bg-opacity-50">
         <div className="bg-gray-900 bg-opacity-95 flex flex-col items-center justify-center gap-4 sm:py-8 py-6 sm:px-16 px-12">
           <Spinner />
           <h1 className="sm:text-4xl text-2xl font-audio text-gray-200">Loading...</h1>
         </div>
       </div>
-    </div>
+    </main>
   )
 }

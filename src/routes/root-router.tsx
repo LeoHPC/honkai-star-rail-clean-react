483186a2bbde7e4f2b99e170bceb8ce110dddb76
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-11 18:32:37 -0300

    refactor: changing routes file to config folder

--- src/routes/root-router.tsx
+++ src/routes/root-router.tsx
@@ -1,4 +1,4 @@
-import { Routes } from '@/routes'
+import { Routes } from '@/config/routes'
 import { RouteObject, useRoutes } from 'react-router-dom'
 
 export const routeList: RouteObject[] = [...Routes]

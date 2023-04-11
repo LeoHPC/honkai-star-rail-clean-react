de4edb7c060b5265f6ce3a727995e41dc4fbfc28
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-11 18:26:39 -0300

    refactor: removing ellipsis from loading

--- src/presentation/pages/home/components/loading-background/loading-background.tsx
+++ src/presentation/pages/home/components/loading-background/loading-background.tsx
@@ -9,7 +9,7 @@ export const LoadingBackground = () => {
       <div className="border-4 border-red-600 p-1 bg-gray-200 bg-opacity-50">
         <div className="bg-gray-900 bg-opacity-95 flex flex-col items-center justify-center gap-4 sm:py-8 py-6 sm:px-16 px-12">
           <Spinner />
-          <h1 className="sm:text-4xl text-2xl font-audio text-gray-200">Loading...</h1>
+          <h1 className="sm:text-4xl text-2xl font-audio text-gray-200">Loading</h1>
         </div>
       </div>
     </main>

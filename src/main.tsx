e7f8f7d768c4188c64a303cd47fcf00d9ab1001f
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-14 23:48:27 -0300

    feat: use query in home page

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import '@/shared/styles/global.css'
 
 import { RootRouter } from './config/routes/root-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <RootRouter />
-    </QueryClientProvider>
+    <RootRouter />
   </BrowserRouter>
 )
--- src/main/factory/pages/home-factory.tsx
+++ src/main/factory/pages/home-factory.tsx
@@ -1,8 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 // Component
 import { Home } from '@/presentation/pages'
 // Factories
 import { factoryGetCharactersDataUseCase } from '@/main/factory/usecases'
 
+const queryClient = new QueryClient()
+
 export const factoryHomePage = (): JSX.Element => {
-  return <Home getCharactersDataUseCase={factoryGetCharactersDataUseCase()} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home getCharactersDataUseCase={factoryGetCharactersDataUseCase()} />
+    </QueryClientProvider>
+  )
 }

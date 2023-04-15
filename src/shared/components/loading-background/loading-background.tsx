39f9bb5406a3dda2a4b54fb24f1ea981e0bbffc1
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-15 16:45:33 -0300

    feat: adding background color on loading background component

--- src/shared/components/loading-background/loading-background.tsx
+++ src/shared/components/loading-background/loading-background.tsx
@@ -4,7 +4,7 @@ import { LoadingSpinner } from '@/shared/components'
 export const LoadingBackground = () => {
   return (
     <main
-      className="bg-loading-background bg-top bg-cover bg-no-repeat min-h-screen flex justify-center items-center flex-col"
+      className="bg-loading-background bg-top bg-cover bg-no-repeat bg-zinc-900 min-h-screen flex justify-center items-center flex-col"
       data-testid="loading-page">
       <div className="border-4 border-red-600 p-1 bg-gray-200 bg-opacity-50">
         <div className="bg-gray-900 bg-opacity-95 flex flex-col items-center justify-center gap-4 sm:py-8 py-6 sm:px-16 px-12">

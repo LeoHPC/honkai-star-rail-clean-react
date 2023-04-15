77cdce37cb43e33025ffa800b1bb273b079fcd14
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-14 23:26:25 -0300

    chore: setting react query

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import '@/shared/styles/global.css'
 
 import { RootRouter } from './config/routes/root-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <RootRouter />
+    <QueryClientProvider client={queryClient}>
+      <RootRouter />
+    </QueryClientProvider>
   </BrowserRouter>
 )

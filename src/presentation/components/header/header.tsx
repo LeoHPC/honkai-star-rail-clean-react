3795f6025719dce68f9a34061af16dc116549d1f
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-15 15:23:29 -0300

    feat: header component

---
+++ src/presentation/components/header/header.tsx
@@ -0,0 +1,30 @@
+// Components
+import { ExternalHeaderLink, InternalHeaderLink } from '@/shared/components'
+
+export const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <ExternalHeaderLink
+              label="official website"
+              href="https://hsr.hoyoverse.com/en-us/home?utm_source=hsrofficalweb&utm_medium=fab&utm_campaign=button"
+              aria-label="official website (open in a new tab)"
+            />
+          </li>
+          <li>
+            <ExternalHeaderLink
+              label="register now"
+              href="https://hsr.hoyoverse.com/en-us/"
+              aria-label="register now (open in a new tab)"
+            />
+          </li>
+          <li>
+            <InternalHeaderLink label="characters" href="#characters" aria-label="come see our beautiful characters" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}

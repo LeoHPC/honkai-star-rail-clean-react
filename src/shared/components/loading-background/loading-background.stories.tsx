6b08985eb2cf700fdea5faa0ff8c27ec40bc7e84
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-16 19:16:29 -0300

    feat: documenting loading background component

---
+++ src/shared/components/loading-background/loading-background.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { LoadingBackground } from './loading-background'
+
+export default {
+  title: 'Components/Loading Background',
+  component: LoadingBackground
+} as Meta
+
+export const Default: StoryObj = {}

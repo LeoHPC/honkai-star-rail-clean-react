365effc0c10e99c03d4d47baf6156f51d7f8d45a
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-16 16:13:01 -0300

    feat: documenting loading spinner component

---
+++ src/shared/components/loading-spinner/loading-spinner.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { LoadingSpinner } from './loading-spinner'
+
+export default {
+  title: 'Components/Loading Spinner',
+  component: LoadingSpinner
+} as Meta
+
+export const Default: StoryObj = {}

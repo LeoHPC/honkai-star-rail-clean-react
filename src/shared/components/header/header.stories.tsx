57983405ffde21c8f71f9022c57d09d170c644f0
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-16 17:03:17 -0300

    feat: documenting header component

---
+++ src/shared/components/header/header.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Header } from './header'
+
+export default {
+  title: 'Components/Header',
+  component: Header
+} as Meta
+
+export const Default: StoryObj = {}

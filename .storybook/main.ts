bdb91dd56e7db34b0938f65b2ade6938c0bd647a
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-16 16:02:02 -0300

    chore: setting storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import { mergeConfig } from 'vite'
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  core: {
+    builder: '@storybook/builder-vite'
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      optimizeDeps: {
+        include: ['storybook-dark-mode']
+      }
+    })
+  }
+}
+export default config

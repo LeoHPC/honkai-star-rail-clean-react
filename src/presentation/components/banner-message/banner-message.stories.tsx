9c128743971c3f213e1ae4104c66cacf089f99cf
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-18 19:00:19 -0300

    feat: documenting banner message component

---
+++ src/presentation/components/banner-message/banner-message.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { BannerMessage } from './banner-message'
+
+export default {
+  title: 'Presentation/Banner Message',
+  component: BannerMessage
+} as Meta
+
+export const Default: StoryObj = {}

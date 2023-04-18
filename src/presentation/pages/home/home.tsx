25d791ef3476d31007a62d50993841a7e1f347bd
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-18 18:45:34 -0300

    refactor: video background color

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -24,11 +24,11 @@ export const Home = (props: HomeProps): JSX.Element => {
     <div>
       <div className="relative w-full h-screen flex items-center justify-center overflow-hidden">
         <div className="absolute top-0 left-0 w-full">
-          <div className="absolute top-0 left-0 bg-zinc-900 bg-opacity-40 w-full h-screen" />
+          <div className="absolute top-0 left-0 bg-black bg-opacity-40 w-full h-screen" />
           <video autoPlay loop muted playsInline className="object-cover w-full h-screen" aria-hidden="true">
             <source src="./video/background-video.mp4#t=0.1" type="video/mp4" />
           </video>
-          <p className="text-gray-200 bg-zinc-900 bg-opacity-40 absolute bottom-0 right-2" aria-hidden="true">
+          <p className="text-gray-200 bg-black bg-opacity-40 absolute bottom-0 right-2" aria-hidden="true">
             Game engine footage showing character Blade
           </p>
         </div>

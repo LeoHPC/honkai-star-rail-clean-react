81b59193b6e7edacb900ef3074acf75c8cdea7f4
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-18 19:09:00 -0300

    feat: documenting home characters section component

---
+++ src/presentation/components/home-characters-section/home-characters-section.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { HomeCharactersSection } from './home-characters-section'
+
+export default {
+  title: 'Presentation/Home Characters Section',
+  component: HomeCharactersSection
+} as Meta
+
+export const Default: StoryObj = {}

139441b33b730dc820c1aec145e87521cc7c62da
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-18 18:54:00 -0300

    feat: documenting background video component

---
+++ src/presentation/components/background-video/background-video.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { BackgroundVideo } from './background-video'
+
+export default {
+  title: 'Presentation/Background Video',
+  component: BackgroundVideo
+} as Meta
+
+export const Default: StoryObj = {}

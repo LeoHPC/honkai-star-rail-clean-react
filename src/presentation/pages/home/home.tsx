9cfeef1131ac2f0d9688014c110ad71f1e72a8b9
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-18 00:11:49 -0300

    feat: play button icon

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -5,6 +5,7 @@ import { Header, LoadingBackground } from '@/shared/components'
 // Types
 import { HomeProps } from './types'
 import { CharactersDataProps } from '@/domain/models'
+import { PlayButtonIcon } from '@/presentation/assets'
 
 const useHome = ({ getCharactersDataUseCase }: HomeProps) => {
   const query = useQuery<CharactersDataProps[], Error>(['charactersData'], async () => {
@@ -36,11 +37,19 @@ export const Home = (props: HomeProps): JSX.Element => {
           <div className="absolute left-0 top-0 w-full">
             <Header />
           </div>
-          <h1
-            className="lg:text-6xl md:text-4xl text-2xl text-center px-4 font-audio text-gray-200 bg-zinc-900 bg-opacity-60 p-8 font-medium w-screen"
-            data-testid="home-message">
-            May This Journey Lead Us Starward
-          </h1>
+          <section className="bg-gray-950 w-screen p-8 flex flex-col items-center justify-center gap-6 bg-opacity-40">
+            <button
+              aria-label="Watch our gameplay trailer on YouTube!"
+              title="Watch our gameplay trailer on YouTube!"
+              className="hover:brightness-110 transition-all border-none outline-none focus-visible:outline-red-600 focus-visible:outline-4">
+              <PlayButtonIcon />
+            </button>
+            <h1
+              className="lg:text-6xl md:text-4xl text-2xl text-center font-audio text-gray-200 font-medium"
+              data-testid="home-message">
+              May This Journey Lead Us Starward
+            </h1>
+          </section>
         </div>
       </div>
       <h1 className="z-10">next content</h1>

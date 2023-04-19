a102e6f92bc679d0be5e9dd43b6a8ba54f6444f9
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-18 23:58:17 -0300

    refactor: adding background color on home

--- src/presentation/components/background-video/background-video.tsx
+++ src/presentation/components/background-video/background-video.tsx
@@ -1,11 +1,11 @@
 export const BackgroundVideo = () => {
   return (
-    <div className="absolute top-0 left-0 w-full">
-      <div className="absolute top-0 left-0 bg-black bg-opacity-40 w-full h-screen" />
+    <div className="absolute top-0 left-0 w-full bg-black">
+      <div className="absolute top-0 left-0 bg-zinc-900 bg-opacity-40 w-full h-screen" />
       <video autoPlay loop muted playsInline className="object-cover w-full h-screen" aria-hidden="true">
         <source src="./video/background-video.mp4#t=0.1" type="video/mp4" />
       </video>
-      <p className="text-gray-200 bg-black bg-opacity-40 absolute bottom-0 right-2" aria-hidden="true">
+      <p className="text-gray-200 bg-zinc-900 bg-opacity-40 absolute bottom-0 right-2" aria-hidden="true">
         Game engine footage showing character Blade
       </p>
     </div>

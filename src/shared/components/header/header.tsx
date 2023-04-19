91d672f62be81e9402f811655f2d79fc88d5d1d1
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-19 00:04:46 -0300

    refactor: header styles

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -3,9 +3,9 @@ import { ExternalHeaderLink, InternalHeaderLink } from '@/shared/components'
 
 export const Header = () => {
   return (
-    <header className="absolute left-0 top-0 w-full">
+    <header className="absolute left-0 top-0 w-full bg-gray-900 bg-opacity-50 px-4 py-2">
       <nav>
-        <ul>
+        <ul className="flex flex-row gap-8 justify-end">
           <li>
             <ExternalHeaderLink
               label="official website"
--- src/ui/header-link/header-link.tsx
+++ src/ui/header-link/header-link.tsx
@@ -4,7 +4,7 @@ import { HeaderLinkProps } from './types'
 export const HeaderLink = ({ label, children, ...props }: HeaderLinkProps) => {
   return (
     <a
-      className="text-xl flex flex-row items-center gap-1 font-semibold tracking-widest uppercase text-yellow-600 hover:brightness-125 transition-all"
+      className="md:text-lg text-base flex flex-row items-center gap-1 font-semibold tracking-widest uppercase text-yellow-600 hover:brightness-125 transition-all"
       {...props}>
       {label}
       {children}

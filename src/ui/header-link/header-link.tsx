9ca4f19aba116405743345eaa5de679d4924f09a
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-18 23:53:53 -0300

    refactor: making header link accept children

--- src/ui/header-link/header-link.tsx
+++ src/ui/header-link/header-link.tsx
@@ -1,12 +1,13 @@
 // Types
 import { HeaderLinkProps } from './types'
 
-export const HeaderLink = ({ label, ...props }: HeaderLinkProps) => {
+export const HeaderLink = ({ label, children, ...props }: HeaderLinkProps) => {
   return (
     <a
-      className="text-xl font-semibold tracking-widest uppercase text-yellow-600 hover:brightness-125 transition-all"
+      className="text-xl flex flex-row items-center gap-1 font-semibold tracking-widest uppercase text-yellow-600 hover:brightness-125 transition-all"
       {...props}>
       {label}
+      {children}
     </a>
   )
 }

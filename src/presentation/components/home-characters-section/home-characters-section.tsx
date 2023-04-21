59c033d0b5813c80ae5a4e1d60c3f644f052fb00
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-21 00:44:35 -0300

    refactor: centralizing background

--- src/presentation/components/home-characters-section/home-characters-section.tsx
+++ src/presentation/components/home-characters-section/home-characters-section.tsx
@@ -1,6 +1,6 @@
 export const HomeCharactersSection = () => {
   return (
-    <main id="characters" className="h-screen w-full bg-gray-900 bg-characters-background bg-cover bg-no-repeat">
+    <main id="characters" className="h-screen w-full bg-gray-900 bg-characters-background bg-cover bg-no-repeat bg-top">
       <h2 className="z-10 text-gray-200 text-2xl">next content</h2>
     </main>
   )

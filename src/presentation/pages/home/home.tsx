944c613a51fe4df468a2c3003de36b951df87f0a
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-21 01:27:02 -0300

    fix: skip to main content contrast

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -26,7 +26,7 @@ export const Home = (props: HomeProps): JSX.Element => {
         href="#characters"
         className="absolute top-0 left-[9999px] focus-visible:left-0 z-30 outline-none border-none focus-visible:outline-4 focus-visible:outline-fuchsia-500">
         <div className="absolute bg-gray-900 w-64 h-64 top-[-8rem] left-[-6rem] rounded-full border-4 border-fuchsia-500" />
-        <span className="absolute text-gray-200 w-64 top-4 left-4 font-audio text-4xl flex-nowrap">
+        <span className="absolute text-gray-200 w-80 bg-gray-900 bg-opacity-70 border-r-4 py-1 border-r-fuchsia-500 pr-4 top-4 left-4 font-audio text-4xl flex-nowrap">
           Skip to Main Content
         </span>
       </a>

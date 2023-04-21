53b4824cdf71bbd4ec9809b6d2ad203d16747c38
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-21 01:21:28 -0300

    feat: skip to main content link

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -21,7 +21,15 @@ export const Home = (props: HomeProps): JSX.Element => {
   if (query.isLoading) return <LoadingBackground />
 
   return (
-    <div>
+    <div className="relative overflow-hidden">
+      <a
+        href="#characters"
+        className="absolute top-0 left-[9999px] focus-visible:left-0 z-30 outline-none border-none focus-visible:outline-4 focus-visible:outline-fuchsia-500">
+        <div className="absolute bg-gray-900 w-64 h-64 top-[-8rem] left-[-6rem] rounded-full border-4 border-fuchsia-500" />
+        <span className="absolute text-gray-200 w-64 top-4 left-4 font-audio text-4xl flex-nowrap">
+          Skip to Main Content
+        </span>
+      </a>
       <HomeFirstSection />
       <HomeCharactersSection />
     </div>

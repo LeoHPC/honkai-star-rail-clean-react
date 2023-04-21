5030557dc9810d6bbf4e0cb026bdbb571c560270
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-21 19:37:11 -0300

    feat: character section responsiveness

--- src/presentation/components/home-characters-section/home-characters-section.tsx
+++ src/presentation/components/home-characters-section/home-characters-section.tsx
@@ -9,9 +9,11 @@ export const HomeCharactersSection = ({ characterData, characters, setCurrentCha
   return (
     <main
       id="characters"
-      className="relative min-h-screen w-full bg-gray-900 bg-characters-background bg-cover bg-no-repeat bg-top flex flex-row items-center">
-      <h2 className="absolute top-4 left-4 font-audio italic text-6xl font-medium text-gray-200">Characters</h2>
-      <section className="w-full min-h-screen flex items-center justify-center">
+      className="relative min-h-screen w-full bg-gray-900 bg-characters-background bg-cover bg-no-repeat bg-top flex flex-col lg:flex-row items-center">
+      <h2 className="absolute top-0 left-0 p-4 sm:p-0 sm:top-4 sm:left-4 font-audio italic text-4xl sm:text-6xl font-medium text-gray-200">
+        Characters
+      </h2>
+      <section className="w-full h-full py-6 px-2 sm:p-0 sm:min-h-screen flex items-center justify-center">
         <img
           src={characterData.image}
           alt={`Character ${characterData.name} of Honkai Star Rail`}
@@ -19,8 +21,10 @@ export const HomeCharactersSection = ({ characterData, characters, setCurrentCha
           height="100%"
         />
       </section>
-      <section className="w-full min-h-screen bg-black bg-opacity-80 flex flex-col justify-center p-16">
-        <h3 className="font-audio font-medium text-8xl text-gray-200 tracking-widest" data-testid="character-name">
+      <section className="w-full h-full lg:min-h-screen bg-black bg-opacity-80 flex flex-col justify-center p-6 sm:p-12 md:p-16">
+        <h3
+          className="font-audio font-medium text-6xl md:text-8xl text-gray-200 tracking-widest"
+          data-testid="character-name">
           {characterData.name}
         </h3>
         <div className="w-48 h-1 bg-gray-200 my-8" />
@@ -31,7 +35,7 @@ export const HomeCharactersSection = ({ characterData, characters, setCurrentCha
             </span>
           ))}
         </div>
-        <span className="text-2xl text-gray-200 tracking-wider font-semibold flex flex-row items-center gap-2">
+        <span className="text-lg sm:text-2xl text-gray-200 tracking-wider font-semibold flex flex-row items-center gap-2">
           <img
             src={ELEMENT_IMAGES[characterData.element].image}
             alt={ELEMENT_IMAGES[characterData.element].alt}
@@ -40,10 +44,14 @@ export const HomeCharactersSection = ({ characterData, characters, setCurrentCha
           />
           {characterData.element}
         </span>
-        <span className="text-2xl text-gray-200  tracking-wider font-semibold">VA: {characterData.va}</span>
-        <cite className="mt-4 text-xl text-gray-200 font-medium tracking-wide">- {characterData.description}</cite>
-        <h4 className="font-audio font-medium text-3xl text-gray-200 tracking-widest mt-12">Select Your Character</h4>
-        <div className="flex flex-row items-center gap-2">
+        <span className="text-lg sm:text-2xl text-gray-200  tracking-wider font-semibold">VA: {characterData.va}</span>
+        <cite className="mt-4 text-base sm:text-xl text-gray-200 font-medium tracking-wide h-32 lg:h-full overflow-y-auto scrollbar-hide">
+          - {characterData.description}
+        </cite>
+        <h4 className="font-audio font-medium text-xl sm:text-3xl text-gray-200 tracking-widest mt-4 sm:mt-12">
+          Select Your Character
+        </h4>
+        <div className="flex flex-row items-center gap-2 w-full">
           {characters.map((item) => (
             <button
               key={item.id}

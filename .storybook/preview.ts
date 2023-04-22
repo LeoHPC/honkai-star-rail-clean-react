638b5521733ca5d04a2adbffcd8731fb0e716324
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-22 19:23:53 -0300

    fix: storybook deploy

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,6 +6,7 @@ import '../src/shared/styles/global.css'
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    previewTabs: { 'storybook/docs/panel': { index: -1 } },
     controls: {
       matchers: {
         color: /(background|color)$/i,

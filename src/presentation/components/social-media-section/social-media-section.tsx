1d449e225a817a49f59dcac7bf60b85bbe78ade6
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-22 15:01:57 -0300

    refactor: using social media section row

--- src/presentation/components/social-media-section/social-media-section.tsx
+++ src/presentation/components/social-media-section/social-media-section.tsx
@@ -6,7 +6,7 @@ import { SOCIAL_MEDIA_DATA } from '@/utils/constants'
 export const SocialMediaSection = () => {
   return (
     <section
-      className="absolute bottom-0 lef-0 sm:right-0 px-1 py-2 bg-gray-900 bg-opacity-50 flex flex-row sm:flex-col gap-3"
+      className="absolute bottom-0 lef-0 sm:right-0 px-1 py-2 bg-gray-900 bg-opacity-50 flex flex-row md:flex-col gap-3"
       aria-label="Follow us on our social media!">
       {SOCIAL_MEDIA_DATA.map((media) => (
         <SocialMediaIcon key={media.name} aria={media.aria} href={media.href} icon={media.icon} name={media.name} />

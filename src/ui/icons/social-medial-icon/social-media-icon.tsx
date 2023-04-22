e0ec291820db66965e50f70ee663e0cbf34095e7
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-22 16:06:22 -0300

    feat: focus visible on social media links

--- src/ui/icons/social-medial-icon/social-media-icon.tsx
+++ src/ui/icons/social-medial-icon/social-media-icon.tsx
@@ -3,7 +3,13 @@ import { Props } from './types'
 
 export const SocialMediaIcon = ({ aria, href, icon, name }: Props) => {
   return (
-    <a href={href} aria-label={aria} title={name} target="_blank" rel="noopener noreferrer">
+    <a
+      href={href}
+      aria-label={aria}
+      title={name}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="outline-none border-none focus-visible:outline-4 focus-visible:outline-fuchsia-500">
       {icon}
     </a>
   )

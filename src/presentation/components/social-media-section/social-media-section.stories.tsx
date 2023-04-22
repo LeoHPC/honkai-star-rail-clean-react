7246bb6d8e7a8f54c8bb347cfd1571de89bb6f7b
Leonardo Henrique Petreca Costa <leonardo.costa@kabum.com.br>
2023-04-22 00:30:43 -0300

    docs: documenting social media section

---
+++ src/presentation/components/social-media-section/social-media-section.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { SocialMediaSection } from './social-media-section'
+
+export default {
+  title: 'Presentation/Social Media Section',
+  component: SocialMediaSection
+} as Meta
+
+export const Default: StoryObj = {}
